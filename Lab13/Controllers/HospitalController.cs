50fa864a88acc319fb0ad6ffb7e08a11eb51cf87
NikitaKrutsenKo <niankr5@gmail.com>
2024-11-22 18:49:03 +0200

    Lab 13 update structure

---
+++ Lab13/Controllers/HospitalController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using Lab13.Lab6GetToken;
+
+namespace Lab13.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HospitalController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public HospitalController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Hospital>>> GetHospitals()
+        {
+            var client = _httpClientFactory.CreateClient("HospitalApiClient");
+
+            var token = await GetToken.GetAccessTokenAsync(_httpClientFactory, _configuration);
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync("Hospital");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return StatusCode((int)response.StatusCode, $"API error: {response.StatusCode}");
+            }
+
+            var hospitals = await response.Content.ReadFromJsonAsync<List<Hospital>>();
+            return Ok(hospitals);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Hospital>> GetHospital(int id)
+        {
+            var client = _httpClientFactory.CreateClient("HospitalApiClient");
+
+            var token = await GetToken.GetAccessTokenAsync(_httpClientFactory, _configuration);
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync($"Hospital/{id}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return NotFound("Hospital not found!");
+            }
+
+            var hospital = await response.Content.ReadFromJsonAsync<Hospital>();
+            return Ok(hospital);
+        }
+    }
+
+}
--- Lab13/Program.cs
+++ Lab13/Program.cs
@@ -3,6 +3,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -12,7 +22,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseCors("AllowReactApp");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
